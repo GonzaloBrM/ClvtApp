55a037079c5274935bea34cc58d2305615a4ccfb
GonzaloBrm <gonzalobrama@gmail.com>
2020-03-31 15:57:25 +0200

    Add REST

---
+++ clvt-rest/clvt/src/main/java/com/gbm/clvt/ClvtApplication.java
@@ -0,0 +1,13 @@
+package com.gbm.clvt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClvtApplication.class, args);
+	}
+
+}
---
+++ clvt-rest/clvt/src/test/java/com/gbm/clvt/ClvtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbm.clvt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClvtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
